3d04e820b20e99500bfa100d7051aba5890d9af5
Mert-Kuzucu <mert.kuzucu@elev.ga.ntig.se>
2023-09-18 08:54:57 +0200

    förra lektionen

---
+++ src/hero.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class hero {
+
+    Scanner scan = new Scanner(System.in);
+
+public static void main(String[] args){
+
+
+    Hero hero = new Hero();
+}}
